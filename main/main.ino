c2bee1835f51d766cc9e9b3c4a0a793fca6719ef
noahschn05 <noahs0510@gmail.com>
2025-11-04 17:51:16 -0500

    Code for motor
    Co-authored-by: Siam <Siammmmmmm@users.noreply.github.com>

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,42 @@
 #include "mbed.h"
 
-void setup()
-{
+// --- Pin Definitions ---
+#define STEP_PIN 2
+#define DIR_PIN 3
+#define ENABLE_PIN 4
+
+void setup() {
+    pinMode(STEP_PIN, OUTPUT);
+    pinMode(DIR_PIN, OUTPUT);
+    pinMode(ENABLE_PIN, OUTPUT);
     
+    // Enable driver (LOW = ON for DRV8825)
+    digitalWrite(ENABLE_PIN, LOW);
+
+    // Set intitial direction
+    digitalWrite(DIR_PIN, HIGH);
 }
 
-void loop()
-{
-    
+void loop() {
+    // Rotate one direction
+    digitalWrite(DIR_PIN, HIGH);
+    for (int i = 0; i < 200; i++) {     // 200 steps = 1 revolution
+        digitalWrite(STEP_PIN, HIGH);
+        delayMicroseconds(1000);        // pulse width
+        digitalWrite(STEP_PIN, LOW);
+        delayMicroseconds(1000);
+    }
+
+    delay(1000);    // pause 1 second
+
+    // Rotate the other direction
+    digitalWrite(DIR_PIN, LOW);
+    for (int i = 0; i < 200; i++) {
+        digitalWrite(STEP_PIN, HIGH);
+        delayMicroseconds(1000);
+        digitalWrtie(STEP_PIN, LOW);
+        delayMicroseconds(1000);
+    }
+
+    delay(1000);
 }
