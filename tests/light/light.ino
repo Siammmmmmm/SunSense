7eaadb34d285c7119c1ec631a81f8b8085164887
noahschn05 <noahs0510@gmail.com>
2025-11-11 17:00:52 -0500

    Update light.ino

--- tests/light/light.ino
+++ tests/light/light.ino
@@ -1,12 +1,37 @@
-#include <ts>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
 
 Adafruit_TSL2561_Unified light = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
-void setup()
-{
-    
+void setup() {
+    Serial.begin(9600);
+    delay(1000);
+
+    Serial.println("Starting TSL2561 test...");
+
+    // --- Initialize TSL2561 ---
+    if(!light.begin()) {
+        Serial.println("TSL2561 not found!");
+        while (1); // stop here if not found;
+    } else {
+        Serial.println("TSL2561 found!");
+        light.enableAutoRange(true);
+        light.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
+     }
 }
-void loop()
-{
-    
-}
+
+void loop() {
+    // --- Read TSL2561 ---
+    sensors_event_t event;
+    light.getEvent(&event);
+    if (event.light) {
+        Serial.print("Light: ");
+        Serial.print(event.light);
+        Serial.println(" lux");
+    } else {
+        serial.println("No light data");
+    } 
+
+    delay(1000); // read every second
+}    
